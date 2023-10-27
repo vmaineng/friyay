36c20cc61ffcaf77caa36b629630be8d33fa3994
Mai Vang <vmaineng@gmail.com>
2023-10-26 17:31:29 -0700

    added in Card format

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { AspectRatio, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
 
 export const Video = () => {
   return (
     <div>
-        
-        </div>
+      <Card maxW='sm'>
+        <CardBody>
+          <AspectRatio maxW='560px' ratio={1}>
+            <iframe
+              title='naruto'
+              src='https://www.youtube.com/embed/QhBnZ6NPOY0'
+              allowFullScreen
+            />
+          </AspectRatio>
+        </CardBody>
+      </Card>
+    </div>
   )
 }
