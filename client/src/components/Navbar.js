f7c5dceda4ad44d29d5f2efbb7eeff7b93e78ea0
Mai Vang <vmaineng@gmail.com>
2023-11-06 19:35:38 -0600

    added in children

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -24,6 +24,50 @@ import {
 export const Navbar = () => {
   const { isOpen, onToggle } = useDisclosure();
 
+  const linkColor = useColorModeValue('gray.600', 'gray.200')
+  const linkHoverColor = useColorModeValue('gray.800', 'white')
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800')
+
+  const NAV_ITEMS: Array<NavItem> = [
+    {
+      label: 'Inspiration',
+      children: [
+        {
+          label: 'Explore Design Work',
+          subLabel: 'Trending Design to inspire you',
+          href: '#',
+        },
+        {
+          label: 'New & Noteworthy',
+          subLabel: 'Up-and-coming Designers',
+          href: '#',
+        },
+      ],
+    },
+    {
+      label: 'Find Work',
+      children: [
+        {
+          label: 'Job Board',
+          subLabel: 'Find your dream design job',
+          href: '#',
+        },
+        {
+          label: 'Freelance Projects',
+          subLabel: 'An exclusive list for contract work',
+          href: '#',
+        },
+      ],
+    },
+    {
+      label: 'Learn Design',
+      href: '#',
+    },
+    {
+      label: 'Hire Designers',
+      href: '#',
+    },
+  ]
   
 
   return (
@@ -41,7 +85,46 @@ export const Navbar = () => {
           align={"center"}
         >
           Navigation
-        
+          <Stack direction={'row'} spacing={4}>
+          {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Box
+                as="a"
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}>
+                {navItem.label}
+              </Box>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}>
+                {/* <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack> */}
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+
+            </Stack>
 
         <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
           <Text
