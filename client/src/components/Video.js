c413dfc2610bd895dfa07c51624896ef42ff29ef
Mai Vang <vmaineng@gmail.com>
2023-11-06 21:42:34 -0600

    added in description

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -8,6 +8,7 @@ import {
   Stack,
   CardBody,
   CardFooter,
+  useColorModeValue,
 } from "@chakra-ui/react";
 // import {
 //   Carousel,
@@ -23,41 +24,84 @@ export const Video = () => {
 
   return (
     <div>
-       <Container maxW={'7xl'} py={16} as={Stack} spacing={12}>
-        <Stack spacing={0} align={'center'}>
+      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={"center"}>
           <Heading>Our Clients Speak</Heading>
           <Text>We have been working with clients around the world</Text>
         </Stack>
-      <Stack
-          direction={{ base: 'column', md: 'row' }}
-          spacing={{ base: 10, md: 4, lg: 10 }}>
-            
-      <Card maxW="sm">
-        <CardBody>
-          <AspectRatio maxW="560px" ratio={1}>
-            <iframe
-              title="naruto"
-              src="https://www.youtube.com/embed/QhBnZ6NPOY0"
-              allowFullScreen
-            />
-          </AspectRatio>
-        </CardBody>
-        {/* </Card>
+        <Stack
+          direction={{ base: "column" }}
+          spacing={{ base: 10, md: 4, lg: 10 }}
+        >
+          <Card
+            bg={useColorModeValue("white", "gray.800")}
+            boxShadow={"lg"}
+            p={8}
+            rounded={"xl"}
+            align={"center"}
+            pos={"relative"}
+            _after={{
+              content: `""`,
+              w: 0,
+              h: 0,
+              borderLeft: "solid transparent",
+              borderLeftWidth: 16,
+              borderRight: "solid transparent",
+              borderRightWidth: 16,
+              borderTop: "solid",
+              borderTopWidth: 16,
+              borderTopColor: useColorModeValue("white", "gray.800"),
+              pos: "absolute",
+              bottom: "-16px",
+              left: "50%",
+              transform: "translateX(-50%)",
+            }}
+          >
+            {/* Heading for the title */}
+            <Heading as={"h3"} fontSize={"xl"}>
+              Naruto
+            </Heading>
+            {/* Description */}
+            <Text
+              textAlign={"center"}
+              color={useColorModeValue("gray.600", "gray.400")}
+              fontSize={"sm"}
+            >
+              Anime
+            </Text>
+            <AspectRatio maxW="560px" ratio={1}>
+              <iframe
+                title="naruto"
+                src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+                allowFullScreen
+              />
+            </AspectRatio>
+          </Card>
 
-      <Card maxW='sm'> */}
-        <CardBody>
-          <AspectRatio maxW="560px" ratio={1}>
-            <iframe
-              title="naruto"
-              src="https://www.youtube.com/embed/QhBnZ6NPOY0"
-              allowFullScreen
-            />
-          </AspectRatio>
-        </CardBody>
-      </Card>
-      </Stack>
+          <Card>
+            <AspectRatio maxW="560px" ratio={1}>
+              <iframe
+                title="naruto"
+                src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+                allowFullScreen
+              />
+            </AspectRatio>
+          </Card>
+        </Stack>
 
-      {/* <Carousel>
+        <Card maxW="sm">
+          <CardBody>
+            <AspectRatio maxW="560px" ratio={1}>
+              <iframe
+                title="naruto"
+                src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+                allowFullScreen
+              />
+            </AspectRatio>
+          </CardBody>
+        </Card>
+
+        {/* <Carousel>
       <CarouselItems>
         {images.map((image, index) => {
           return (
@@ -67,8 +111,8 @@ export const Video = () => {
           );
         })}
       </CarouselItems> */}
-      {/* <Toolbar /> */}
-      {/* </Carousel> */}
+        {/* <Toolbar /> */}
+        {/* </Carousel> */}
       </Container>
     </div>
   );
